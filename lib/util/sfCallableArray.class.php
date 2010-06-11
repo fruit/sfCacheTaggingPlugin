a54d8a62d7c64ba53501826a02372d5d87ff6424
Ilya Sabelnikov <fruit.dev@gmail.com>
2010-06-12 01:07:53 +0300

    Added customized sfCallable class

---
+++ lib/util/sfCallableArray.class.php
@@ -0,0 +1,28 @@
+<?php
+
+  /*
+   * This file is part of the sfCacheTaggingPlugin package.
+   * (c) 2009-2010 Ilya Sabelnikov <fruit.dev@gmail.com>
+   *
+   * For the full copyright and license information, please view the LICENSE
+   * file that was distributed with this source code.
+   */
+
+  /**
+   * Added callArray method to pass arguments not separatly, but as array
+   *
+   * @package sfCacheTaggingPlugin
+   * @author Ilya Sabelnikov <fruit.dev@gmail.com>
+   */
+  class sfCallableArray extends sfCallable
+  {
+    public function callArray ($arguments)
+    {
+      if (! is_callable($this->getCallable()))
+      {
+        call_user_func_array(array('parent', 'call'), $arguments);
+      }
+
+      return call_user_func_array($this->callable, $arguments);
+    }
+  }
