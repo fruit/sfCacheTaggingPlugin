b20d0dbddcf4e51ae940f6f39980cc9bbfa13efa
Ilya Sabelnikov <fruit.dev@gmail.com>
2011-02-10 00:32:39 +0200

    Finished UpdatingInvalidatesOnlySelfObjectTest

--- test/functional/frontend/UpdatingInvalidatesOnlySelfObjectTest.php
+++ test/functional/frontend/UpdatingInvalidatesOnlySelfObjectTest.php
@@ -50,27 +50,70 @@
   # obtainCollectionVersion 5
   # Delete with SoftDelete
   $eggsCollectionVersion = $eggs->obtainCollectionVersion();
-  $connection->beginTransaction('pre_delete');
-
   $eggs->delete();
   
   $t->cmp_ok($eggsCollectionVersion, '<', $banana->obtainCollectionVersion(), 'Banana collection version increased when some other Food object is deleted (object)');
-  $connection->rollback('pre_delete');
-
+  
   # obtainCollectionVersion 6
 
   $bananaCollectionVersion = $banana->obtainCollectionVersion();
-  $connection->beginTransaction('pre_delete');
+
+  $eggs = new Food();
+  $eggs->setTitle('Eggs')->save();
+
   $q = FoodTable::getInstance()->createQuery();
-  $t->is($q->delete()->where('title = ?', 'Eggs')->execute(), 0, 'SoftDelete sets deleted_at (no physical deletion');
+  $t->is($q->delete()->where('title = ?', 'Eggs')->execute(), 1, 'SoftDelete sets deleted_at (no physical deletion');
   $t->is(FoodTable::getInstance()->findOneByTitle('Eggs'), false, 'Teoreticaly it is removed');
 
   $t->cmp_ok($bananaCollectionVersion, '<', $banana->obtainCollectionVersion(), 'Banana collection version increased when some other Food object is deleted (DQL)');
-  $connection->rollback('pre_delete');
 
   # Delete without SoftDelete behavior
   /**
    * @todo add tests with "Book" table, object and DQL deletes
    */
+
+  $beforeVersion = sfCacheTaggingToolkit::generateVersion();
+
+  $bible = new Book();
+  $bible->setLang('en');
+  $bible->setSlug('the-holy-bible');
+  $bible->save();
+
+  $afterVersion = sfCacheTaggingToolkit::generateVersion();
+
+  $bibleCollectionVersion = $bible->obtainCollectionVersion();
+
+  $t->cmp_ok($bibleCollectionVersion, '>', $beforeVersion, 'Bible collection version is greater then microtime taken before');
+  $t->cmp_ok($bibleCollectionVersion, '<', $afterVersion, 'Bible collection version is less then microtime taken after');
+
+  $meinKampf = new Book();
+  $meinKampf->setLang('de');
+  $meinKampf->setSlug('mein-kampf');
+  $meinKampf->save();
+
+  $t->cmp_ok($bibleCollectionVersion, '<', $meinKampf->obtainCollectionVersion(), 'By adding new Book, collection version increases');
+
+  $meinKampfCollectionVersion = $meinKampf->obtainCollectionVersion();
+
+  $meinKampf->setLang('de_CH');
+  $meinKampf->save();
+
+  $t->is($meinKampfCollectionVersion, $meinKampf->obtainCollectionVersion(), 'By updating existing Book, collection version stays unchanged');
+  $meinKampfCollectionVersion = $meinKampf->obtainCollectionVersion();
+  $meinKampf->delete();
+
+  $t->cmp_ok($meinKampfCollectionVersion, '<', $bible->obtainCollectionVersion(), 'By removing Book, collection version increases');
+
+  $bibleCollectionVersion = $bible->obtainCollectionVersion();
+
+  $meinKampf = new Book();
+  $meinKampf->setLang('de');
+  $meinKampf->setSlug('mein-kampf');
+  $meinKampf->save();
+
+  $q = BookTable::getInstance()->createQuery();
+  $t->is($q->delete()->where('slug = ?', 'mein-kampf')->execute(), 1, 'DQL removing Book "mein-kampf"');
+
+  $t->cmp_ok($bibleCollectionVersion, '<', $bible->obtainCollectionVersion(), 'By removing Book using DQL, collection version increases');
 
   $connection->rollback();
