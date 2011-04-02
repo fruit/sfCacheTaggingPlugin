2eefa89000b6937e0f7ee2ad08b1919bc67fff8b
Ilya Sabelnikov <fruit.dev@gmail.com>
2011-04-03 02:21:53 +0300

    Added default admin generator to cache backend part

---
+++ data/generator/sfDoctrineModule/cachable/template/templates/_list_field_boolean.php
@@ -0,0 +1,5 @@
+[?php if ($value): ?]
+  [?php echo image_tag(sfConfig::get('sf_admin_module_web_dir').'/images/tick.png', array('alt' => __('Checked', array(), 'sf_admin'), 'title' => __('Checked', array(), 'sf_admin'))) ?]
+[?php else: ?]
+  [?php print image_tag('/media/images/backend/cross.png', array('alt' => __('Unchecked', array(), 'sf_admin'), 'title' => __('Unchecked', array(), 'sf_admin'))) ?]
+[?php endif; ?]
