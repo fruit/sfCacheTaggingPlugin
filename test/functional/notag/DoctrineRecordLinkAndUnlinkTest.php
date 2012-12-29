e537365fd626672da855f55ad897e0c0ccf91518
Ilya Sabelnikov <fruit.dev@gmail.com>
2012-12-29 19:58:02 +0200

    Updates all file headers (c) end-years to 2013

--- test/functional/notag/DoctrineRecordLinkAndUnlinkTest.php
+++ test/functional/notag/DoctrineRecordLinkAndUnlinkTest.php
@@ -1,7 +1,7 @@
 <?php
   /*
    * This file is part of the sfCacheTaggingPlugin package.
-   * (c) 2009-2012 Ilya Sabelnikov <fruit.dev@gmail.com>
+   * (c) 2009-2013 Ilya Sabelnikov <fruit.dev@gmail.com>
    *
    * For the full copyright and license information, please view the LICENSE
    * file that was distributed with this source code.
